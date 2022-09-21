97d74b863b01498584c96d3147062a8bb8ee5074
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-09-21 18:56:59 -0300

    first commit

---
+++ bin/tef_backend.dart
@@ -0,0 +1,5 @@
+import 'package:tef_backend/tef_backend.dart' as tef_backend;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tef_backend.calculate()}!');
+}
---
+++ test/tef_backend_test.dart
@@ -0,0 +1,8 @@
+import 'package:tef_backend/tef_backend.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
